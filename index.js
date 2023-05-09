65bd49e43f219be3ab31c457e905223b5fbdfdfc
TianDatong <temberature@gmail.com>
2023-05-09 23:18:00 +0800

    getActiveBrowserURL

--- index.js
+++ index.js
@@ -32,17 +32,53 @@ ipcMain.on('get-summary', (event) => {
   const pythonScriptPath = '/Users/tong/SDD/contextChat/html_to_markdown.py';
   const virtualEnvPath = '/Users/tong/SDD/contextChat/venv/bin/python';
 
-  // Replace the following URL with the URL of the web page you want to summarize
-  const theURL = 'https://example.com';
-
-  exec(
-    `${virtualEnvPath} ${pythonScriptPath} ${theURL}`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      event.reply('summary-result', stdout);
+  getActiveBrowserURL((theURL) => {
+    if (theURL === "error") {
+      console.error("Safari 或 Google Chrome 必须处于打开状态。");
+      event.reply('summary-result', "Safari 或 Google Chrome 必须处于打开状态。");
+      return;
     }
-  );
+
+    exec(
+      `${virtualEnvPath} ${pythonScriptPath} ${theURL}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          event.reply('summary-result', `Error: ${error}`);
+          return;
+        }
+        event.reply('summary-result', stdout);
+      }
+    );
+  });
 });
+
+
+
+function getActiveBrowserURL(callback) {
+  const appleScript = `
+  tell application "System Events"
+    set processList to name of every process
+    if "Safari" is in processList then
+      tell application "Safari"
+        set theURL to URL of current tab of window 1
+      end tell
+    else if "Google Chrome" is in processList then
+      tell application "Google Chrome"
+        set theURL to URL of active tab of window 1
+      end tell
+    else
+      set theURL to "error"
+    end if
+  end tell
+  return theURL`;
+
+  exec(`osascript -e '${appleScript}'`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      callback("error");
+      return;
+    }
+    callback(stdout.trim());
+  });
+}
