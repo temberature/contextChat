45f3f056679958d3f3b712bc35fcc30d803deb10
TianDatong <temberature@gmail.com>
2023-05-15 19:48:24 +0800

    redditPost

--- js/handlers.js
+++ js/handlers.js
@@ -1,8 +1,13 @@
 const { ipcRenderer } = require("electron");
-const { getCompletion, getCompletionStream, setLoading } = require("./utils");
+const { getCompletion, getCompletionStream, setLoading, redditPost } = require("./utils");
+const { clipboard } = require('electron');
+
+
 var vex = require('vex-js')
 vex.registerPlugin(require('vex-dialog'))
 vex.defaultOptions.className = 'vex-theme-os'
+
+
 
 function handleButtonPress(buttonKey) {
     switch (buttonKey) {
@@ -10,9 +15,10 @@ function handleButtonPress(buttonKey) {
             createDialogues();
             break;
         case "4":
-            ipcRenderer.send("get-summary");
+            summarize();
+
             break;
-        
+
 
     }
 }
@@ -24,7 +30,7 @@ async function createDialogues() {
         placeholder: 'Planet name',
         callback: async function (value) {
             console.log(value)
-            const prompt = `Could you please create three brief, hypothetical one-round dialogues in the style of the characters from the TV show "Person of Interest" with background, where each dialogue incorporates "${value}"?\n\n`;
+            const prompt = `Could you please create three brief, hypothetical, one-round dialogues in the style of the characters from the TV show "Person of Interest" with background, where each dialogue incorporates "${value}"?\n\n`;
             document.getElementById("result").innerText += prompt;
             const response = await getCompletionStream(
                 prompt,
@@ -36,23 +42,46 @@ async function createDialogues() {
             );
         }
     })
-    
+
 }
 
-ipcRenderer.on("markdown-excerpt", async (event, markdownExcerpt) => {
-    const prompt = markdownExcerpt.replaceAll(' ', '') + "总结一下上述内容：";
-    console.log(prompt);
-    const response = await getCompletionStream(
-        prompt,
-        "gpt-3.5-turbo",
-        (chunk) => {
-            setLoading(false);
-            document.getElementById("result").innerText += chunk;
-        }
-    );
-
-    // Handle the response as needed, e.g., display it in the web page
+ipcRenderer.on("markdown-contexts", async (event, contexts) => {
+    for (let i = 0; i < contexts.length; i++) {
+        const context = contexts[i];
+        const prompt = context + ",summarize above content：";
+        console.log(prompt);
+        const response = await getCompletionStream(
+            prompt,
+            "gpt-3.5-turbo",
+            (chunk) => {
+                setLoading(false);
+                document.getElementById("result").innerText += chunk;
+            }
+        );
+        document.getElementById("result").innerText += "\n\n";
+    }
+    redditPost();
 })
+
+async function summarize() {
+    let content = clipboard.readText();
+
+    if (content && content.length > 1000) {
+        const prompt = content + ", summarize above content:";
+        console.log(prompt);
+        const response = await getCompletionStream(
+            prompt,
+            "gpt-3.5-turbo",
+            (chunk) => {
+                setLoading(false);
+                document.getElementById("result").innerText += chunk;
+            }
+        );
+        document.getElementById("result").innerText += "\n\n";
+    } else {
+        ipcRenderer.send("get-summary");
+    }
+}
 
 module.exports = {
     handleButtonPress,
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,6 @@
+const snoowrap = require('snoowrap');
+const fetch = require('node-fetch');
+
 function setLoading(loading) {
     const loadingElement = document.getElementById("loading");
     if (loading) {
@@ -34,62 +37,91 @@ async function getCompletionStream(
     model = "gpt-3.5-turbo",
     callback
 ) {
-    const requestBody = {
-        model: model,
-        messages: [{ role: "user", content: prompt }],
-        temperature: 0,
-        stream: true,
-    };
+    return new Promise((resolve, reject) => {
 
-    const source = new SSE(`https://api.openai.com/v1/chat/completions`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer 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`,
-        },
-        payload: JSON.stringify(requestBody),
-    });
-    let first = true,
-        chunk;
-    source.addEventListener("error", (e) => {
-        console.log(e);
-        const data = JSON.parse(e.data || "{}");
-        if (data.error) {
-            console.log(data.error);
-            let confirmation;
-            if (data.error.code === "context_length_exceeded") {
-                alert(`${JSON.stringify(data.error)}`);
-            } else {
-                confirmation = window.confirm(
-                    `${JSON.stringify(data.error)}\n\nWould you like to try again?`
-                );
-            }
-        }
-    });
-    source.addEventListener("message", function (e) {
-        // Assuming we receive JSON-encoded data payloads:
-        console.log(e.data);
-        if (e.data !== "[DONE]") {
-            var data = JSON.parse(e.data);
-            // console.log(data);
+        const requestBody = {
+            model: model,
+            messages: [{ role: "user", content: prompt }],
+            temperature: 0,
+            stream: true,
+        };
 
-            if (!data.choices[0].delta.content) {
-                return;
+        const source = new SSE(`https://api.openai.com/v1/chat/completions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer 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`,
+            },
+            payload: JSON.stringify(requestBody),
+        });
+        let first = true,
+            chunk;
+        source.addEventListener("error", (e) => {
+            console.log(e);
+            const data = JSON.parse(e.data || "{}");
+            if (data.error) {
+                console.log(data.error);
+                let confirmation;
+                if (data.error.code === "context_length_exceeded") {
+                    alert(`${JSON.stringify(data.error)}`);
+                } else {
+                    confirmation = window.confirm(
+                        `${JSON.stringify(data.error)}\n\nWould you like to try again?`
+                    );
+                }
             }
-            if (first) {
-                chunk = data.choices[0].delta.content;
-                first = false;
+        });
+        source.addEventListener("message", function (e) {
+            // Assuming we receive JSON-encoded data payloads:
+            console.log(e.data);
+            if (e.data !== "[DONE]") {
+                var data = JSON.parse(e.data);
+                // console.log(data);
+
+                if (!data.choices[0].delta.content) {
+                    return;
+                }
+                if (first) {
+                    chunk = data.choices[0].delta.content;
+                    first = false;
+                } else {
+                    chunk = data.choices[0].delta.content;
+                }
+                callback(chunk);
             } else {
-                chunk = data.choices[0].delta.content;
+                resolve();
             }
-            callback(chunk);
-        }
+        });
+        source.stream();
     });
-    source.stream();
+}
+
+function redditPost() {
+    // Replace these with your own values
+    const r = new snoowrap({
+        userAgent: 'your_user_agent',
+        clientId: 'kOMMUHcNrDTmC4w7N4Fg_Q',
+        clientSecret: 'cKIB6DmymUYd9G7TGD23c-zM1BI3fA',
+        username: 'temberatur',
+        password: 'IhAd@!159107'
+    });
+
+    // Define the subreddit you want to post to
+    const subredditName = 'thirdbrain'; // Replace with the subreddit you want
+
+    // Define the link and the title of the post
+    const title = 'Check out this awesome link!';
+    const url = 'https://www.example.com';
+
+    // Post the link
+    r.getSubreddit(subredditName).submitLink({ title: title, url: url })
+        .then(console.log)
+        .catch(console.error);
 }
 
 module.exports = {
     getCompletion,
     getCompletionStream,
     setLoading,
+    redditPost
 };
